633ae95a5c796600889754aff1fcbcd0f06e564e
Abhiram Krishnam <abhikrishnam01@gmail.com>
2025-04-22 15:26:22 -0400

    six

--- app/workspace/_components/EditorExtension.js
+++ app/workspace/_components/EditorExtension.js
@@ -1,16 +1,60 @@
-import { Bold } from 'lucide-react' 
+import { Bold, Italic, Sparkles } from 'lucide-react' 
 import React from 'react'
+import { useParams } from 'next/navigation'
+import { useAction } from 'convex/react'; 
+import { api } from '../../../convex/_generated/api'; // Adjust path if needed
+
 
 function EditorExtension ({editor}) {
-    return (
+
+    const {fileId}=useParams();
+    const SearchAI=useAction(api.myAction.search)
+    const onAiClick=async()=>{
+        const selectedText=editor.state.doc.textBetween(
+            editor.state.selection.from,
+            editor.state.selection.to,
+            ' '
+        );
+        console.log("selectedText", selectedText)
+
+        const result=await SearchAI({
+            query: selectedText,
+            fileId: fileId
+            
+    })
+
+    const UnformattedAns=JSON.parse(result);
+    let AllUnformattedAns='';
+    UnformattedAns&&UnformattedAns.forEach(item=>{
+        AllUnformattedAns=AllUnformattedAns+item.pageContent
+    }) ;
+
+
+    const PROMPT="For question :"+selectedText+" and with the given content as answer"+
+    " please give appropriate answer in HTML format. The answer content is: "+AllUnformattedAns
+}
+    
+    return editor&&(
         <div className='p-5'>
             <div className="control-group"> 
-            <div className="button-group">
+            <div className="button-group flex gap-3">
             <button
                 onClick={()=>editor.chain().focus().toggleBold().run()}
-                className={editor.isActive('bold') ? 'text-blue' : ''} >
+                className={editor.isActive('bold') ? 'text-blue-500' : ''} >
             <Bold/>
             </button>
+            <button
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            className={editor.isActive('italic') ? 'text-blue-500' : ''}
+          >
+            <Italic/>
+          </button>
+          <button
+            onClick={() => onAiClick() }
+            className={''}
+          >
+            <Sparkles/>
+          </button>
             </div>
             </div>
         </div>
--- convex/myAction.js
+++ convex/myAction.js
@@ -25,3 +25,25 @@ export const ingest = action({
     return "Completed.."
   },
 });
+
+export const search = action({
+  args: {
+    query: v.string(),
+    fileId:v. string()
+  },
+  handler: async (ctx, args) => {
+    const vectorStore = new ConvexVectorStore(
+      new GoogleGenerativeAIEmbeddings ({
+        apiKey:'AIzaSyCw9MFSunrQVuiIWBBPNtpwGg_rsau5guo',
+        model: "text-embedding-004", // 768 dimensions 
+        taskType: TaskType.RETRIEVAL_DOCUMENT,
+        title: "Document title",
+        }),
+        { ctx });
+
+    const resultOne = (await vectorStore.similaritySearch(args.query, 1)).filter((q=>q.metadata.fileId==args.fileId));
+    console.log(resultOne);
+
+    return JSON.stringify(resultOne);
+  },
+});
